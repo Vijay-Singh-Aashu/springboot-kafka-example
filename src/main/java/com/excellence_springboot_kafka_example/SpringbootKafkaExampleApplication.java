c4991ae7acc442e37207175234493ff415c0020b
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-11 18:09:28 +0530

    initial-commit

---
+++ src/main/java/com/excellence_springboot_kafka_example/SpringbootKafkaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_kafka_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence_springboot_kafka_example/SpringbootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_kafka_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
